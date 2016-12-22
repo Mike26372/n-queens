5a16c5acb61ee5183549cb40cfdf9c0fdfa6eec8
Mike26372 <tutt.michael@gmail.com>
2016-12-21 20:34:22 -0800

    Trying to debug

--- src/solvers.js
+++ src/solvers.js
@@ -28,15 +28,19 @@ window.findNRooksSolution = function(n) {
   var board = new Board({'n': n});
   // console.log(board.rows());
   var results = [];
+  var tempRow = 0;
+  var tempCol = 0;
 
   var checkNewBoard = function(newBoard) {
-    //debugger;
     var count = newBoard.rows().reduce((acc, curr) => acc + curr.reduce((acc2, curr2) => acc2 + curr2, 0), 0);
     var length = newBoard.rows().length;
+
     if (count === n) {
-      results = newBoard.rows();
-      return results;
+      results.push(newBoard.rows());
+      console.log(tempRow, tempCol);
+      //newBoard.togglePiece(tempRow, tempCol);
     }
+
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length; j++) {
         let currentBoard = newBoard.rows().slice();
@@ -45,7 +49,9 @@ window.findNRooksSolution = function(n) {
         if (tempBoard.rows()[i][j] === 1) {
           continue;
         }
+
         tempBoard.togglePiece(i, j);
+
         if (!tempBoard.hasAnyRooksConflicts()) {
           checkNewBoard(tempBoard);
         } else {
@@ -55,18 +61,18 @@ window.findNRooksSolution = function(n) {
     }
   };
 
-
   checkNewBoard(board);
-  console.log('57 ' + results);
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(results));
-  return results;
+  //console.log('Single solution for ' + n + ' rooks:', JSON.stringify(results));
+  console.log('solutions ' + results);
+  return results[0];
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
   var solutionCount = findNRooksSolution(n);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  console.log(solutionCount);
+  return solutionCount.length;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
